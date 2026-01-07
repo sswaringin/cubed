7cb0953abe97eb9eec3b877af07aed7f69eb7900
Stephen Swaringin <stephen.swaringin@gmail.com>
2026-01-07 08:35:50 -0600

    writing base files

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,7 +1,6 @@
 import { Command } from "commander";
 import * as p from "@clack/prompts";
-import { runCommand, writeDir } from "../utils/common";
-import path from "node:path";
+import { runCommand, writeDir, writeFile } from "../utils/common";
 
 export const create = new Command("create")
   .description("scaffolds a new project")
@@ -19,11 +18,21 @@ export const create = new Command("create")
         "./utilities",
       ];
       baseDirs.forEach((dir) => writeDir(directory, dir));
+
+      // TODO: convert baseFiles to key value pairs to link each path to its content
+      // TODO: consider more efficient approaches compared to manually tracking all file paths
+      // TODO: when do I update the "global.css" file? after each base file is created? Don't assume things will always succeed.
+      const baseFiles = [
+        "./global.css",
+        "./global/global-styles.css",
+        "./global/reset.css",
+      ];
+      baseFiles.forEach((file) => writeFile(directory, file, "testing123"));
     });
   });
 
 async function makeDirs() {
-  await p.text({ message: "hello" });
+  await p.text({ message: "making dirs" });
 }
 
 async function createProject() {
